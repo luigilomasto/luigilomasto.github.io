617493ea51aea608a241e1258d375b5f6dfc9d70
Luigi <luigilomasto@gmail.com>
2022-01-13 21:56:36 +0100

    update

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,7 @@
 <?php
   $receiving_email_address = 'luigilomasto@gmail.com';
-  $result = mail($receiving_email_address, $_POST['subject'], $_POST['message'],  "FROM: $_POST[email]");
+  $headers = 'From:' .  $_POST['email'] . "\r\n" .
+    'Reply-To: carlomagno@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+  $result = mail($receiving_email_address, $_POST['subject'], $_POST['message'],  $headers);
 ?>
